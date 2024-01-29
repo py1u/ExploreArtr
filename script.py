d3ee8ae3d50ddf941296c61cc70ea723cf216b03
py1u <peter.lu723@gmail.com>
2024-01-28 21:43:26 -0800

    added assests,html/css files

--- script.py
+++ script.py
@@ -1,51 +1,54 @@
-html_code = '''
-<div class="col-sm-6 col-md-4 col-lg-3 col-xxl-2">
-    <div class="card border-0 rounded-4 mb-3">
-        {img_tag}
-        <div class="card-user"> 
-            <div class="card-user mt-1 ms-3"><strong>ArtrCrab</strong></div>
-            <h5 class="card-text mb-1 ms-3">Name of the art piece or small comment</h5>
-        </div> 
+def generate_html():
+    html_code = '''
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Image Gallery</title>
+        <link rel="stylesheet" href="styles.css"> <!-- Link to your CSS file -->
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                {image_cards}
+            </div>
+        </div>
+    </body>
+    </html>
+    '''
+    return html_code
+
+def save_html_file(html_code, filename="output.html"):
+    with open(filename, "w") as file:
+        file.write(html_code)
+    print(f"HTML code saved to {filename}")
+
+if __name__ == "__main__":
+    html_code_template = '''
+    <div class="col-sm-6 col-md-4 col-lg-3 col-xxl-2">
+        <div class="card border-0 rounded-4 mb-3" style="background-color: black;">
+            {img_tag}
+            <div class="card-user"> 
+                <div class="card-user mt-1 ms-3"><strong></strong></div>
+            </div> 
+        </div>
     </div>
-</div>
-'''
+    '''
 
-image_sources = [
-"assets/img17.jpg",
-"assets/img5.jpg",
-"assets/img29.jpg",
-"assets/img8.jpg",
-"assets/img12.jpg",
-"assets/img24.jpg",
-"assets/img2.jpg",
-"assets/img19.jpg",
-"assets/img21.jpg",
-"assets/img14.jpg",
-"assets/img4.jpg",
-"assets/img30.jpg",
-"assets/img10.jpg",
-"assets/img25.jpg",
-"assets/img11.jpg",
-"assets/img1.jpg",
-"assets/img6.jpg",
-"assets/img27.jpg",
-"assets/img16.jpg",
-"assets/img22.jpg",
-"assets/img13.jpg",
-"assets/img9.jpg",
-"assets/img20.jpg",
-"assets/img7.jpg",
-"assets/img18.jpg",
-"assets/img23.jpg",
-"assets/img3.jpg",
-"assets/img26.jpg",
-"assets/img15.jpg",
-"assets/img28.jpg"
-]
+    image_sources = [
+        "assets/img17.jpg", "assets/img5.jpg", "assets/img29.jpg", "assets/img8.jpg", "assets/img12.jpg",
+        "assets/img24.jpg", "assets/img2.jpg", "assets/img19.jpg", "assets/img21.jpg", "assets/img14.jpg",
+        "assets/img4.jpg", "assets/img30.jpg", "assets/img10.jpg", "assets/img25.jpg", "assets/img11.jpg",
+        "assets/img1.jpg", "assets/img6.jpg", "assets/img27.jpg", "assets/img16.jpg", "assets/img22.jpg",
+        "assets/img13.jpg", "assets/img9.jpg", "assets/img20.jpg", "assets/img7.jpg", "assets/img18.jpg",
+        "assets/img23.jpg", "assets/img3.jpg", "assets/img26.jpg", "assets/img15.jpg", "assets/img28.jpg"
+    ]
 
-final_html = ''
-for src in image_sources:
-    img_tag = f'<img src="{src}" class="card-img-top rounded-4 cardy" alt="...">'
-    final_html += html_code.format(img_tag=img_tag)
+    final_image_cards = ''
+    for src in image_sources:
+        img_tag = f'<img src="{src}" class="card-img-top rounded-4 cardy" alt="...">'
+        final_image_cards += html_code_template.format(img_tag=img_tag)
 
-print(final_html)
+    html_page = generate_html().format(image_cards=final_image_cards)
+    save_html_file(html_page)
--- testing.js
+++ testing.js
@@ -1,8 +1,118 @@
-const list = document.querySelectorAll('.list');
-function activeLink() {
-    list.forEach((item) =>
-    item.classList.remove('active'));
-    this.classList.add('active');
+document.addEventListener("DOMContentLoaded", function() {
+    SplitTextIntoSpans(".logo p");
+    SplitTextIntoSpans(".hero-copy h1");
+
+    gsap.to(".img-holder img", {
+        left: 0,
+        stagger: 0.1,
+        ease: "power4.out",
+        duration: 1.5,
+        delay: 4,
+    });
+
+    gsap.to(".img-holder img", {
+        left: "110%",
+        stagger: -0.1,
+        ease: "power4.out",
+        duration: 1.5,
+        delay: 7,
+    });
+});
+
+function SplitTextIntoSpans(selector) {
+    var element = document.querySelector(selector);
+    if (element) {
+        var text = element.innerText;
+        var splitText = text
+            .split("")
+            .map((char) => `<span>${char}</span>`)
+            .join("");
+        element.innerHTML = splitText;
+    }
 }
-list.forEach((item) =>
-item.addEventListener('click',activeLink));
+
+function startLoader() {
+    var counterElement = document.querySelector(".counter p");
+    var logoElement = document.querySelector(".logo p");
+    var currentValue = 0;
+
+    function updateCounter() {
+        if (currentValue === 100) {
+            animateText();
+            return;
+        }
+
+        currentValue += Math.floor(Math.random() * 10) + 1;
+        currentValue = currentValue > 100 ? 100 : currentValue;
+        counterElement.innerHTML =
+            currentValue
+                .toString()
+                .split("")
+                .map((char) => `<span>${char}</span>`)
+                .join("") + "<span>%</span>";
+
+        var delay = Math.floor(Math.random() * 200) + 100;
+        setTimeout(updateCounter, delay);
+    }
+
+    
+
+
+    function animateText() {
+        setTimeout(() => {
+            gsap.to(".counter p span", {
+                top: "-400px",
+                stagger: 0.1,
+                ease: "power3.inOut",
+                duration: 1,
+            });
+
+            gsap.to(".logo p span", {
+                top: "0",
+                stagger: 0.1,
+                ease: "power3.inOut",
+                duration: 1,
+            });
+
+            gsap.to(".logo p span", {
+                top: "-400px",
+                stagger: 0.1,
+                ease: "power3.inOut",
+                duration: 1,
+                delay: 3,
+            });
+
+            gsap.to(".overlay", {
+                opacity: 0,
+                ease: "power3.inOut",
+                duration: 1,
+                delay: 4,
+            });
+
+            gsap.to(".hero img", {
+                scale: 1,
+                ease: "power3.inOut",
+                duration: 2,
+                delay: 3.5,
+            });
+
+            gsap.to(".hero-copy h1 span", { 
+                top: "0",
+                stagger: 0.1,
+                ease: "power3.inOut",
+                duration: 2,
+                delay: 4,
+            });
+
+            gsap.to("nav", {
+                top: "0",
+                ease: "power3.inOut",
+                duration: 2,
+                delay: 4,
+            });
+        }, 300);
+    }
+    updateCounter();
+}
+
+startLoader();
